175f686ac7347eb2710ef30731bc13200baa02a0
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-02-10 18:30:51 -0500

    Worked through indexOf and join

--- indexOf.js
+++ indexOf.js
@@ -1,9 +1,29 @@
 'use strict';
 
+
+// methods used
+console.log("***Methods used!***");
 const arr = [1, 2, 3, 4, 3, 6];
 
 const found = arr.indexOf(3);
 console.log(found);
 
-const found = arr.indexOf(5);
-console.log(found);
+const found2 = arr.indexOf(5);
+console.log(found2);
+
+// my function
+console.log("\n\n***My Function!***");
+
+const findIndex = ((arr, num) => {
+    const arrLength = arr.length;
+    let index = -1;
+    for (let i = 0; i < arrLength; i++) {
+        if (arr[i] === num) {
+            index = i;
+            break;
+        };
+    };
+    return index;
+});
+console.log(findIndex(arr, 5));
+console.log(findIndex(arr, 3));
--- join.js
+++ join.js
@@ -6,4 +6,21 @@ console.log('***methods***');
 const arr = ['j', 'e', 'd', 'i'];
 
 console.log(arr.join(''));
+console.log(arr.join('-'));
 
+// using my function
+console.log("*****my function****");
+
+const arr1 = ['j', 'e', 'd', 'i'];
+
+const joinIt = ((arr, btwn) => {
+    const arrLength = arr.length;
+    let newString = arr[0];
+    for (let i = 1; i < arrLength; i++) {
+        newString += btwn + arr[i];
+    }
+    return newString;
+});
+
+console.log(joinIt(arr1, ""));
+console.log(joinIt(arr1, "-"));
