770b995f6aa91089ebe6e91586a31f7b30fce65e
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-02-17 10:04:07 -0500

    pop started

--- pop.js
+++ pop.js
@@ -1,4 +1,10 @@
 'use strict';
+
+// Methods Used
+console.log("****Methods Used****");
 const arr = [1, 2, 3, 4];
 const remove = arr.pop();
 console.log(remove);
+
+// My Function
+console.log("****My function used****");
